8e5512fc353a511cb8a5c4e9ca369f6974270db6
PravitB <pravit.bhattacharjee2024@ieminternational.ai>
2025-09-04 22:04:51 +0530

    week 6 full

--- Assignments/Sem 3/week 6/Binary Tree LL.c
+++ Assignments/Sem 3/week 6/Binary Tree LL.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct Node
 {
@@ -10,10 +11,12 @@ typedef struct Node
 
 node* createNode(int);
 node* createTree(node*);
+int height(node*);
+void displayLevel(node*, int);
 void displayTree(node*);
-// void inOrder(node*);
-// void preOrder(node*);
-// void postOrder(node*);
+void inOrder(node*);
+void preOrder(node*);
+void postOrder(node*);
 
 void main()
 {
@@ -24,12 +27,12 @@ void main()
     root = createNode(data);
     root = createTree(root);
     displayTree(root);
-    // printf("\nInorder Traversal: ");
-    // inOrder(root);
-    // printf("\nPreorder Traversal: ");
-    // preOrder(root);
-    // printf("\nPostorder Traversal: ");
-    // postOrder(root);
+    printf("\nInorder Traversal: ");
+    inOrder(root);
+    printf("\nPreorder Traversal: ");
+    preOrder(root);
+    printf("\nPostorder Traversal: ");
+    postOrder(root);
 }
 node* createNode(int data)
 {
@@ -58,14 +61,84 @@ node* createTree(node* n)
     }
     return n;
 }
+int height(node* n)
+{
+    if (n == NULL)
+        return 0;
+    else
+    {
+        int lHeight = height(n->left);
+        int rHeight = height(n->right);
+        if (lHeight > rHeight)
+            return lHeight + 1;
+        else
+            return rHeight + 1;
+    }
+}
+void displayLevel(node* n, int level)
+{
+    int nodes= (int)pow(2, level - 1);
+    if (n == NULL)
+        {
+            while (nodes--)
+                printf("E ");
+            return;
+        }
+    if (level == 1)
+        printf("%d ", n->data);
+    else if (level > 1)
+    {
+        displayLevel(n->left, level - 1);
+        displayLevel(n->right, level - 1);
+    }
+}
+
+
 void displayTree(node* n)
+{  
+    int h = height(n);
+    for (int level = 0; level < h; level++) 
+    {
+        int leading=0;
+        if (level == 0) leading = (int)pow(2, h - 1) - 1;
+        else if (level != h-1) leading = 2 *( h - level);
+        for (int s = 0; s < leading; s++) printf(" ");
+        displayLevel(n, level + 1);
+        printf("\n");
+    }
+}
+
+
+void inOrder(node* n)
+{
+    if (n == NULL)
+        return;
+    else
+    {
+        inOrder(n->left);
+        printf("%d ", n->data);
+        inOrder(n->right);
+    }
+}
+void preOrder(node* n)
 {
     if (n == NULL)
         return;
     else
     {
         printf("%d ", n->data);
-        displayTree(n->left);
-        displayTree(n->right);
+        preOrder(n->left);
+        preOrder(n->right);
+    }
+}
+void postOrder(node* n)
+{
+    if (n == NULL)
+        return;
+    else
+    {
+        postOrder(n->left);
+        postOrder(n->right);
+        printf("%d ", n->data);
     }
 }
